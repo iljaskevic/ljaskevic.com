66e0a78b33cec5e2d3eda656e4d45698a6ff9082
Igor Ljaskevic <iljaskevic@architech.ca>
2018-03-21 21:18:29 -0400

    Added existing site to repo

---
+++ index.php
@@ -0,0 +1,344 @@
+<!DOCTYPE HTML>
+<!--
+	Miniport by HTML5 UP
+	html5up.net | @n33co
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Igor Ljaskevic Portfolio</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<!--[if lte IE 8]><script src="css/ie/html5shiv.js"></script><![endif]-->
+		<script src="js/jquery.min.js"></script>
+		<script src="js/jquery.scrolly.min.js"></script>
+		<script src="js/skel.min.js"></script>
+		<script src="js/init.js"></script>
+		<noscript>
+			<link rel="stylesheet" href="css/skel.css" />
+			<link rel="stylesheet" href="css/style.css" />
+			<link rel="stylesheet" href="css/style-desktop.css" />
+		</noscript>
+		<!--[if lte IE 8]><link rel="stylesheet" href="css/ie/v8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="css/ie/v9.css" /><![endif]-->
+	</head>
+	<body>
+
+		<!-- Nav -->
+			<nav id="nav">
+				<ul class="container">
+					<li><a href="#top">Top</a></li>
+					<li><a href="#education">Education</a></li>
+					<li><a href="#work">Work</a></li>
+					<li><a href="#projects">Projects</a></li>
+					<li><a href="#contact">Contact</a></li>
+				</ul>
+			</nav>
+
+		<!-- Home -->
+			<div class="wrapper style1 first">
+				<article class="container" id="top">
+					<div class="row">
+						<div class="4u">
+							<span class="image fit"><img src="images/portfolio_pic3.jpg" alt="" height="300" /></span>
+						</div>
+						<div class="8u">
+							<header>
+								<h1><strong>Igor Ljaskevic</strong></h1>
+								<h3>Full-Stack Developer/Network Engineer/Tinkerer</h3>
+							</header>
+							<p>is a Computer Scientist and a Network Engineering Enthusiast who likes to play around with electronics/robotics and IoT in his spare time.</p>
+							<a href="#work" class="button big scrolly">Learn about what I do</a>
+						</div>
+					</div>
+				</article>
+			</div>
+
+		<!-- Education -->
+			<div class="wrapper style2">
+				<article id="education">
+					<header>
+						<h2>Academic accomplishments.</h2>
+						<p>Igor's education and certificates achieved.</p>
+					</header>
+					<div class="container">
+						<div class="row">
+							<div class="4u">
+								<section class="box style1">
+									<!--<span class="icon featured ryerson-crest"></span>-->
+									<img src="images/ryerson_crest.jpg" height="200"></img>
+									<h3>BSc. in Computer Science Co-op</h3>
+									<p>Graduated from Computer Science undergraduate program in 2012 at Ryerson University with 3.26 Cumulative GPA.</p>
+								</section>
+							</div>
+							<div class="4u">
+								<section class="box style1">
+									<!--<span class="icon featured ryerson-crest"></span>-->
+									<img src="images/ryerson_crest.jpg" height="200"></img>
+									<h3>MEng. in Computer Networks</h3>
+									<p>Graduated from Computer Networks Master's of Engineering program in 2014 at Ryerson University with 3.40 Cumulative GPA.</p>
+								</section>
+							</div>
+							<div class="4u">
+								<section class="box style1">
+									<span class="icon featured fa-thumbs-o-up"></span>
+									<h3>Certification</h3>
+									<p>None yet, but several planned. <br>(Cisco, VMware, RedHat, and Microsoft)</p>
+								</section>
+							</div>
+						</div>
+					</div>
+					<footer>
+						<p>Lorem ipsum dolor sit sapien vestibulum ipsum primis?</p>
+						<a href="#projects" class="button big scrolly">See some of my projects</a>
+					</footer>
+				</article>
+			</div>
+
+		<!-- Work -->
+			<div class="wrapper style2">
+				<article id="work">
+					<header>
+						<h2>Here's what he does.</h2>
+						<p>Current and previous work experience.</p>
+					</header>
+					<div class="container"><div class="row">
+							<div class="6u">
+								<section class="box style1">
+									<img src="images/architech_logo.png"></img>
+									<!--<span class="icon featured fa-comments-o"></span>-->
+									<h3>Intermediate Developer</h3>
+									<h4>[Mar. 2015 - Current]</h4>
+									<p>Delivery, Architech</p>
+								</section>
+								
+							</div>
+							<div class="6u">
+								<section class="box style1">
+									<img src="images/architech_logo.png"></img>
+									<!--<span class="icon featured fa-comments-o"></span>-->
+									<h3>Junior Developer</h3>
+									<h4>[Dec. 2014 - Mar. 2015]</h4>
+									<p>Innovation Lab, Architech</p>
+								</section>
+								
+							</div>
+						</div>
+						<div class="row">
+							<div class="6u">
+								<section class="box style1">
+									<!--<span class="icon featured fa-comments-o"></span>-->
+									<img src="images/ryerson_logo.png" width="200"></img>
+									<h3>Web/Streaming Media Developer</h3>
+									<h4>[May 2011 - Dec 2014]</h4>
+									<p>Computing And Communication Services, Ryerson University</p>
+								</section>
+							</div>
+							<div class="6u">
+								<section class="box style1">
+									<!--<span class="icon featured fa-camera-retro"></span>-->
+									<img src="images/ryerson_logo.png" width="200"></img>
+									<h3>Junior Programmer</h3>
+									<h4>[Jan 2011 – May 2011]</h4>
+									<p>Web Services, University Advancement, Ryerson University</p>
+								</section>
+							</div>
+						</div>
+						<div class="row">
+							<div class="6u">
+								<section class="box style1">
+									<!--<span class="icon featured fa-thumbs-o-up"></span>-->
+									<img src="images/cibc_logo.jpg" width="215"></img>
+									<h3>Application Developer</h3>
+									<h4>[May 2010 - Aug 2010]</h4>
+									<p>Corporate Systems & Enterprise Shared Services, CIBC</p>
+								</section>
+							</div>
+							<div class="6u">
+								<section class="box style1">
+									<img src="images/cihi_logo.jpg" width="190"></img>
+									<h3>Application Developer</h3>
+									<h4>[May 2009 - April 2010]</h4>
+									<p>IT Architecture and Standards, CIHI</p>
+								</section>
+							</div>
+							<!--<div class="4u">
+								<section class="box style1">
+									<img src="images/ryerson_logo.png" width="200"></img>
+									<h3>Lorem dolor tempus</h3>
+									<p>Ornare nulla proin odio consequat sapien vestibulum ipsum primis sed amet consequat lorem dolore.</p>
+								</section>
+							</div>
+							<div class="4u">
+								<section class="box style1">
+									<h3>Java Developer</h3>
+									<p>Ornare nulla proin odio consequat sapien vestibulum ipsum primis sed amet consequat lorem dolore.</p>
+								</section>
+							</div>-->
+						</div>
+					</div>
+					<footer>
+						<p>Download full resume in PDF: INSERT_LINK</p>
+						<a href="#projects" class="button big scrolly">See some of my recent work</a>
+					</footer>
+				</article>
+			</div>
+
+		<!-- Projects-->
+			<div class="wrapper style3">
+				<article id="projects">
+					<header>
+						<h2>Here are some current/previous/future projects.</h2>
+						<p>Projects done in school or in spare time at home.</p>
+					</header>
+					<div class="container">
+						<div class="row">
+							<div class="4u">
+								<article class="box style2">
+									<a href="http://projects.ljaskevic.com/?portfolio=quad-copter" class="image featured"><img src="images/pic01.jpg" alt="" /></a>
+									<h3><a href="http://projects.ljaskevic.com/?portfolio=quad-copter">Quad-Copter</a></h3>
+									<h4>[Current]</h4>
+									<p>Arduino Quad-Copter controlled by Android over Bluetooth.</p>
+								</article>
+							</div>
+							<div class="4u">
+								<article class="box style2">
+									<a href="http://projects.ljaskevic.com/?portfolio=raspberry-pi-power-module" class="image featured"><img src="images/pic02.jpg" alt="" /></a>
+									<h3><a href="http://projects.ljaskevic.com/?portfolio=raspberry-pi-power-module">RPi Power Module</a></h3>
+									<h4>[Current]</h4>
+									<p>Multi-Raspberry Pi Power Supply board with Power Buttons.</p>
+								</article>
+							</div>
+							<div class="4u">
+								<article class="box style2">
+									<a href="http://projects.ljaskevic.com/?portfolio=home-data-center" class="image featured"><img src="images/pic03.jpg" alt="" /></a>
+									<h3><a href="http://projects.ljaskevic.com/?portfolio=home-data-center">Home Server Farm</a></h3>
+									<h4>[Current]</h4>
+									<p>ESXi virtualization and SAN storage server deployments for home use.</p>
+								</article>
+							</div>
+						</div>
+						<div class="row">
+							<div class="4u">
+								<article class="box style2">
+									<a href="http://projects.ljaskevic.com/?portfolio=openstack-ha-and-load-balancing" class="image featured"><img src="images/pic04.jpg" alt="" /></a>
+									<h3><a href="http://projects.ljaskevic.com/?portfolio=openstack-ha-and-load-balancing">Masters Project</a></h3>
+									<p>High Availability and Load Balancing in OpenStack Cloud.</p>
+								</article>
+							</div>
+							<div class="4u">
+								<article class="box style2">
+									<a href="http://projects.ljaskevic.com/?portfolio=synchronized-raspberry-pi-displays" class="image featured"><img src="images/pic05.jpg" alt="" /></a>
+									<h3><a href="http://projects.ljaskevic.com/?portfolio=synchronized-raspberry-pi-displays">Synced RPi Displays</a></h3>
+									<p>Play multiple (different) synchronized videos using Raspberry Pies.</p>
+								</article>
+							</div>
+							<div class="4u">
+								<article class="box style2">
+									<a href="#" class="image featured"><img src="images/pic06.jpg" alt="" /></a>
+									<h3><a href="#">Robotics Final Project</a></h3>
+									<p>Maze navigating and candle extinguishing robot.</p>
+								</article>
+							</div>
+						</div>
+					</div>
+					<footer>
+						<p>Have some questions for me?</p>
+						<a href="#contact" class="button big scrolly">Get in touch with me</a>
+					</footer>
+				</article>
+			</div>
+
+		<!-- Contact -->
+			<div class="wrapper style4">
+				<article id="contact" class="container 75%">
+					<header>
+						<h2>Contact me.</h2>
+						<p>If you would like to get in touch with me, use the form below.</p>
+<?php
+if (!empty($error_msg)) {
+	echo '<p class="error">ERROR: '. implode("<br />", $error_msg) . "</p>";
+}
+if ($result != NULL) {
+	echo '<p class="success">'. $result . "</p>";
+}
+?>
+					</header>
+					<div>
+						<div class="row">
+							<div class="12u">
+								<form method="post" action="/mail.php">
+<noscript>
+		<p><input type="hidden" name="nojs" id="nojs" /></p>
+</noscript>
+									<div>
+										<div class="row">
+											<div class="6u">
+												<input type="text" name="name" id="name" placeholder="Name" />
+											</div>
+											<div class="6u">
+												<input type="text" name="email" id="email" placeholder="Email" />
+											</div>
+										</div>
+										<div class="row">
+											<div class="12u">
+												<input type="text" name="subject" id="subject" placeholder="Subject" />
+											</div>
+										</div>
+										<div class="row">
+											<div class="12u">
+												<textarea name="message" id="message" placeholder="Message"></textarea>
+											</div>
+										</div>
+										<div class="row 200%">
+											<div class="12u">
+												<ul class="actions">
+													<li><input type="submit" value="Send Message" /></li>
+													<li><input type="reset" value="Clear Form" class="alt" /></li>
+												</ul>
+											</div>
+										</div>
+									</div>
+								</form>
+							</div>
+						</div>
+						<div class="row">
+							<div class="12u">
+								<hr />
+								<h3>Find me on ...</h3>
+								<ul class="social">
+									<!--<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+									<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+									<li><a href="#" class="icon fa-dribbble"><span class="label">Dribbble</span></a></li>
+									-->
+									<li><a href="#" class="icon fa-linkedin"><span class="label">LinkedIn</span></a></li>
+									<!--<li><a href="#" class="icon fa-tumblr"><span class="label">Tumblr</span></a></li>
+									<li><a href="#" class="icon fa-google-plus"><span class="label">Google+</span></a></li>
+									<li><a href="#" class="icon fa-github"><span class="label">Github</span></a></li>
+									
+									<li><a href="#" class="icon fa-rss"><span>RSS</span></a></li>
+									<li><a href="#" class="icon fa-instagram"><span>Instagram</span></a></li>
+									<li><a href="#" class="icon fa-foursquare"><span>Foursquare</span></a></li>
+									<li><a href="#" class="icon fa-skype"><span>Skype</span></a></li>
+									<li><a href="#" class="icon fa-soundcloud"><span>Soundcloud</span></a></li>
+									<li><a href="#" class="icon fa-youtube"><span>YouTube</span></a></li>
+									<li><a href="#" class="icon fa-blogger"><span>Blogger</span></a></li>
+									<li><a href="#" class="icon fa-flickr"><span>Flickr</span></a></li>
+									<li><a href="#" class="icon fa-vimeo"><span>Vimeo</span></a></li>
+									-->
+								</ul>
+								<hr />
+							</div>
+						</div>
+					</div>
+					<footer>
+						<ul id="copyright">
+							<li>&copy; Igor Ljaskevic. All rights reserved.</li>
+						</ul>
+					</footer>
+				</article>
+			</div>
+
+	</body>
+</html>
